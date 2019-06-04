937f46a4511f10098ea3c77fb91948d37f5a9e7f
Will Gordon <wgordon@redhat.com>
2019-06-04 11:38:14 -0400

    Bump to version 0.3

--- docs/conf.py
+++ docs/conf.py
@@ -23,9 +23,9 @@ copyright = '2019, Will Gordon'
 author = 'Will Gordon'
 
 # The short X.Y version
-version = '0.2'
+version = '0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.3'
 
 
 # -- General configuration ---------------------------------------------------
